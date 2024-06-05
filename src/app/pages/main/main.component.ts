72a4ff2eaaa4a3da0e2f978db17926cb638ad927
Syvets <consaltmdc@gmail.com>
2024-06-05 22:48:14 +0300

    add component and rotes

---
+++ src/app/pages/main/main.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+import { MatButtonModule } from '@angular/material/button';
+import { MatCardModule } from '@angular/material/card';
+import { RouterLink } from '@angular/router';
+
+@Component({
+  selector: 'app-main',
+  standalone: true,
+  imports: [MatCardModule, MatButtonModule, RouterLink],
+  templateUrl: './main.component.html',
+  styleUrl: './main.component.scss',
+})
+export class MainComponent {}
